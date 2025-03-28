cd07cb72583db24536140ac5f66248de62ebc652
nasm1023 <namtl432@gmail.com>
2025-03-28 13:48:18 +0700

    delete fontend, replace with frontend

--- fontend/src/utils/string.util.jsx
+++
@@ -1,7 +0,0 @@
-const removeVietnameseTones = (str) => {
-    return str
-        .normalize("NFD") // Tách dấu khỏi ký tự
-        .replace(/[\u0300-\u036f]/g, "") // Xóa dấu
-        .toLowerCase(); // Chuyển về chữ thường
-};
-export default removeVietnameseTones;
